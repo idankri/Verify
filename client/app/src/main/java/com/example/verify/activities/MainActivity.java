846ffda83f30c339298bc69f816a82c07179a5ec
idankri <58448747+idankri@users.noreply.github.com>
2021-05-01 19:01:57 +0300

    Added initial directories

--- client/app/src/main/java/com/example/verify/activities/MainActivity.java
+++ client/app/src/main/java/com/example/verify/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.verify;
+package com.example.verify.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.verify.R;
 
 public class MainActivity extends AppCompatActivity {
 
